f956e47ae6d46a2d3fab6801d6a24cd81cbdd415
Andre Luis Florencio da Silva <andre.luis.florencio@gmail.com>
2019-06-03 17:45:57 -0300

    Aula 6 - Implementando CRUD na API

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,4 +1,11 @@
+import bodyParser from 'body-parser';
+
 module.exports = app =>{
     app.set("port", 3001);
     app.set("json spaces", 4);
-}
+    app.use(bodyParser.json());
+    app.use((req, res, next)=>{
+        delete req.body.id;
+        next();
+    });
+};
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,61 @@
 module.exports = app =>{
     const Tasks = app.db.models.Tasks
-    app.get('/tasks', (req, res)=>{
-        Tasks.findAll({}).then(tasks =>{
-            res.json({tasks: tasks});
+
+    app.route('/tasks')
+        .get((req, res)=>{
+            // "/tasks": Lista de tarefas
+            Tasks.findAll({})
+                .then(result =>{
+                    res.json(result)
+                })
+                .catch(err =>{
+                    res.status(412).json({msg: err.message});
+                })
+        })
+        .post((req, res)=>{
+            // "/tasks": Cadastra uma nova tarefa
+            Tasks.create(req.body)
+            .then(result =>{
+                res.json(result)
+            })
+            .catch(err=>{
+                res.status(412).json({msg: err.message});
+            })
         });
-    });
-}
+        
+    app.route('/tasks/:id')
+        .get((req, res)=>{
+            // "/tasks/1": Consulta 1 tarefa
+            Tasks.findOne({where: req.params})
+                .then(result=>{
+                    if (result){
+                        res.json(result);
+                    } else {
+                        res.sendStatus(404);
+                    }  
+                })
+                .catch(err=>{
+                    res.status(412).json({msg: err.message});
+                });
+        })
+        .put((req, res)=>{
+            // "/tasks/1": Atualiza 1 tarefa
+            Tasks.update(req.body, {where: req.params})
+                .then(result =>{
+                    res.sendStatus(204);
+                })
+                .catch(err=>{
+                    res.status(412).json({msg: err.message});
+                });
+        })
+        .delete((req, res)=>{
+            // "/tasks/1": Deleta 1 tarefa
+            Tasks.destroy({where: req.params})
+                .then(result=>{
+                    res.sendStatus(204);
+                })
+                .catch(err=>{
+                    res.status(412).json({msg: err.message});
+                });
+        });
+};
