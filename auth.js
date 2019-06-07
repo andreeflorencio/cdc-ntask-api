e73d449d25c6c00d0c967b5c0c97f66a02dc357d
Andre Luis Florencio da Silva <andre.luis.florencio@gmail.com>
2019-06-07 19:24:01 -0300

    Refatorando o código com erro de ortográfia

--- auth.js
+++ auth.js
@@ -3,9 +3,9 @@ import {Strategy, ExtractJwt} from 'passport-jwt';
 
 module.exports = app =>{
     const Users = app.db.models.Users;
-    const cfg = app.libs.confg;
+    const cfg = app.libs.config;
     const params = {
-        secretOrKey = cfg.jwtSecret,
+        secretOrKey: cfg.jwtSecret,
         jwtFromRequest: ExtractJwt.fromAuthHeader()
     };
 
